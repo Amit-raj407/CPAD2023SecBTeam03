2fde48009f3bb05e3b1dfd1329d466c24c97cf34
HellFire03 <dhasalhritika1@gmail.com>
2023-12-09 11:03:51 +0530

    activityIndicator added to cameraScreen

--- components/camera/CameraScreen.style.js
+++ components/camera/CameraScreen.style.js
@@ -50,6 +50,16 @@ const styles = StyleSheet.create({
     marginBottom: "10%",
     justifyContent: 'space-between'
   },
+  loaderContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Darker background
+  }
 });
 
 export default styles;
